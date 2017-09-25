370eae7c0e194669847d13b65e4ae64f1edc3813
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2017-09-24 21:35:45 -0400

    Fixed a problem where background.js functions where not found due to webpack webpackBootstrap

--- src/js/history.js
+++ src/js/history.js
@@ -1,22 +1,27 @@
-let background = browser.extension.getBackgroundPage();
+let backgroundPage = browser.extension.getBackgroundPage();
 
 $(document).ready(function () {
     $('#history').DataTable();
-    //background.load_history();
+    //backgroundPage.load_history();
     $('#refresh-list').click(function (e) {
         e.preventDefault();
-        background.load_history();
+        backgroundPage.load_history(undefined);
     })
 });
 
-browser.runtime.onMessage.addListener(handleMessage);
-
 function handleMessage(request, sender, response) {
+
+    console.log(request);
+    console.log(sender);
+    console.log(response);
+
     if (request.msg === "history-browser-clear-table") {
         // results.innerHTML = "";
     }
     if (request.msg === "history-browser-found-results") {
-        // console.log(request.browserHistory);
-        // $('#history').DataTable(request.browserHistory);
+        console.log(request.browserHistory);
+        $('#history').DataTable(request.browserHistory);
     }
 }
+
+browser.runtime.onMessage.addListener(handleMessage);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require("path");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-        js: "./src/js/history.js",
-        background_scripts: "./src/js/background.js",
+        js: "./src/js/history.js"
     },
     output: {
-        path: path.resolve(__dirname, "addon"),
+        path: path.resolve(__dirname, 'addon'),
         filename: "[name]/main.min.js"
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            // Fixes a problem where functions from a background packed with webpackBootstrap were not defined
+            // I had a hard time figuring this out, but webpack was breaking my background script...
+            // todo: investigate this and pass background.js to the js pipeline instead
+            {from: 'src/js/background.js', to: 'background.js'},
+        ])
+    ]
 };
