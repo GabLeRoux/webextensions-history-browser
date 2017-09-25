f959ebc5ad006f1bea70439bff9ed4e40918d992
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2017-09-24 20:41:57 -0400

    History page is now displaying correctly
    todo: make `load_history` accessible :S

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,14 @@
 async function load_history(query) {
-    browser.runtime.sendMessage({msg: "clear-history-browser-table"});
+    // todo: use `query` for filtering (ex; date range, title, etc.)
+
+    browser.runtime.sendMessage({msg: "history-browser-clear-table"});
 
     var browserHistory = Components.classes["@mozilla.org/browser/nav-history-service;1"]
         .getService(Components.interfaces.nsIBrowserHistory);
 
     console.log(browserHistory);
+
+    browser.runtime.sendMessage({msg: "history-browser-found-results"});
 }
 
 browser.browserAction.onClicked.addListener(() => {
--- src/js/history.js
+++ src/js/history.js
@@ -1,8 +1,22 @@
-var browserHistory = Components.classes["@mozilla.org/browser/nav-history-service;1"]
-    .getService(Components.interfaces.nsIBrowserHistory);
-
-console.log(browserHistory);
+let background = browser.extension.getBackgroundPage();
 
 $(document).ready(function () {
-    $('#hisotry').DataTable();
+    $('#history').DataTable();
+    //background.load_history();
+    $('#refresh-list').click(function (e) {
+        e.preventDefault();
+        background.load_history();
+    })
 });
+
+browser.runtime.onMessage.addListener(handleMessage);
+
+function handleMessage(request, sender, response) {
+    if (request.msg === "history-browser-clear-table") {
+        // results.innerHTML = "";
+    }
+    if (request.msg === "history-browser-found-results") {
+        // console.log(request.browserHistory);
+        // $('#history').DataTable(request.browserHistory);
+    }
+}
